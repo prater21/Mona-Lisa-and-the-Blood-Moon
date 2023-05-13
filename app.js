d42d538f81cc9ed7645a679134abc299dee4b598
Kim Hyeji <prater21@naver.com>
2023-05-13 09:27:34 +0900

    change redirect uri

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, "public")));
 const authEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
 const getTokenEndpoint = "https://oauth2.googleapis.com/token";
-const redirectUri = "http://localhost:3000/MonaLisaAndTheBloodMoon/main";
+// const redirectUri = "http://localhost:3000/MonaLisaAndTheBloodMoon/main";
+const redirectUri = "https://mona-lisa-and-the-blood-moon.onrender.com/MonaLisaAndTheBloodMoon/main"
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const scopes =
@@ -42,6 +43,11 @@ const getReviews = (req, res, next) => {
         });
     
 }
+
+app.get("/", (req, res) => {
+    res.redirect("/MonaLisaAndTheBloodMoon");
+
+});
 
 app.get("/MonaLisaAndTheBloodMoon", (req, res) => {
     res.render("index");
